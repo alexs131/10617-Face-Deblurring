5fb2cdc2750c7ef5c2eab4375da005ac7eb8ac08
ASchneidman <alexmarkschneidman@gmail.com>
2019-11-23 14:52:05 -0500

    discrim

--- final_models/discriminator.py
+++ final_models/discriminator.py
@@ -22,3 +22,13 @@ class Discriminator(nn.Module):
 
     def forward(self, x):
         return self.network.forward(x)
+
+
+# Citation: https://pytorch.org/tutorials/beginner/dcgan_faces_tutorial.html
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        nn.init.normal_(m.weight.data, 0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        nn.init.normal_(m.weight.data, 1.0, 0.02)
+        nn.init.constant_(m.bias.data, 0)
