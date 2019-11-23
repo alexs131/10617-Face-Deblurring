df5bd2825f3470483e9dd14d529f614b076f6069
ASchneidman <alexmarkschneidman@gmail.com>
2019-11-23 12:07:20 -0500

    training discrim

--- final_models/discriminator.py
+++ final_models/discriminator.py
@@ -2,10 +2,10 @@ import torch
 from torch import nn
 
 class Discriminator(nn.Module):
-    def __init__(self, input_shape, num_filters_init):
+    def __init__(self, input_channels, num_filters_init):
         super(Discriminator, self).__init__()
         self.network = nn.Sequential(
-            nn.Conv2d(input_shape[0], num_filters_init, 4, 2, 1),
+            nn.Conv2d(input_channels, num_filters_init, 4, 2, 1),
             nn.LeakyReLU(0.2, True),
             nn.Conv2d(num_filters_init, 2 * num_filters_init, 4, 2, 1),
             nn.BatchNorm2d(2 * num_filters_init),
