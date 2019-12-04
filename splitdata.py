9609553f76f6aeb7d9f0bb5a42723f4baf70de57
alexs131 <alexs1@andrew.cmu.edu>
2019-12-04 18:14:57 -0500

    new splitdat

--- splitdata.py
+++ splitdata.py
@@ -3,7 +3,9 @@ from torchvision.transforms import transforms
 
 from models.LFWC import LFWC
 
-dataset = LFWC(["lfwcrop_color/faces_blurred", "lfwcrop_color/faces_pixelated"], "lfwcrop_color/faces")
+dataset =  LFWC(["lfwcrop_color/faces_blurred0.5","lfwcrop_color/faces_blurred1.0","lfwcrop_color/faces_blurred1.5",
+"lfwcrop_color/faces_blurred2.0","lfwcrop_color/faces_blurred2.5","lfwcrop_color/faces_blurred3.0",
+"lfwcrop_color/faces_pixelated",'lfwcrop_color/faces_linear_motion0','lfwcrop_color/faces_linear_motion90'], "lfwcrop_color/faces")
 train_size = int(0.8 * len(dataset))
 test_size = len(dataset) - train_size
 train_dataset, test_dataset = torch.utils.data.random_split(dataset, [train_size, test_size])
@@ -23,6 +25,6 @@ for im in train_data_loader:
 for im in test_data_loader:
     transform = transforms.ToPILImage()
 
-    transform(im["blurred"][0]).save("test/faces_blurred/" + str(count) + ".ppm")
-    transform(im["nonblurred"][0]).save("test/faces/" + str(count) + ".ppm")
+    transform(im["blurred"][0]).save("data/test/faces_blurred/" + str(count) + ".ppm")
+    transform(im["nonblurred"][0]).save("data/test/faces/" + str(count) + ".ppm")
     count += 1
